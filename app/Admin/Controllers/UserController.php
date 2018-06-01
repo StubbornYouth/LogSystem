964732cd2a3f7a9e89fd8c805b704ac824d66be2
StubbornYouth <757336260@qq.com>
2018-06-01 10:03:10 +0000

    后台管理

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Models\User;
+use Tanmo\Admin\Facades\Admin;
+use Tanmo\Admin\Layout\Content;
+
+class UserController extends Controller
+{
+    public function index(){
+        
+    }
+}
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,7 +5,22 @@ Admin::registerAdminRoutes();
 Route::group([
     'namespace' => 'App\Admin\Controllers',
     'prefix' => 'admin',
-    'middleware' => ['web', 'admin']
+    'middleware' => ['web', 'admin'],
+    'as' => 'admin::'
 ], function () {
-    Route::get('/', 'HomeController@index')->name('admin::main');
+    Route::get('/', 'UserController@index')->name('main');
+
+    ///
+    Route::group([
+    	'middleware' => ['admin.check_permission']
+    ], function(){
+    	//组管理
+    	Route::group([
+    		'namespace' => 'Groups'
+    	],function(){
+    		Route::get('groups','GroupController@index')->name('groups.index');
+    	});
+    });
+
+   
 });
